925940f0991e15654d5425cb73e70e131d2ee997
mrsingh <mrsingh@ncsu.edu>
2015-10-12 22:24:36 -0400

    installed all the dependencies in hw2 folder now

--- HW/HW2/test.js
+++ HW/HW2/test.js
@@ -1,3 +1,6 @@
 var subject = require('./subject.js')
 var mock = require('mock-fs');
+subject.inc(1,undefined);
+subject.inc(1,"mutated123982832AXZ");
 subject.inc(-1,undefined);
+subject.inc(-1,"mutated123982832AXZ");

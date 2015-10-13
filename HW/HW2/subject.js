c876b682fc78aa680387e6bdb3d50ffba8fd6284
mrsingh <mrsingh@ncsu.edu>
2015-10-12 22:48:20 -0400

    recorded the demo

--- HW/HW2/subject.js
+++ HW/HW2/subject.js
@@ -1,9 +1,7 @@
+var fs = require("fs");
 
 function inc(p, q){
-   /* istanbul ignore else  */ 
    if(q ==undefined) q =1;
-   
-   /* istanbul ignore else  */ 
    if( p < 0 )
    {
    	p = -p;
@@ -11,4 +9,97 @@ function inc(p, q){
    return p + q/q;
 }
 
-exports.inc = inc
+function weird(x,y,z, mode)
+{
+    if( x > 7 && y < 0 )
+    {
+        z = 33;
+    }    
+    else if( z < 42 )
+    {
+        if( mode == "strict" )
+        {
+            return 0;
+        }
+        else if( mode.indexOf("werw") == 0 )
+        {
+            return 1;
+        }
+    }
+    else
+    {
+        if( mode != "strict" )
+        {
+            return y = z / x;
+        }
+    }
+    return 1;
+}
+
+
+function fileTest(dir, filePath)
+{
+	if (!fs.existsSync(dir)){
+
+   	    return false;
+    }
+    
+    var files = fs.readdirSync(dir);
+    if( files.length == 0 )
+    {
+        return false;
+    }
+
+   if( fs.existsSync(filePath ))
+   {
+		var buf = fs.readFileSync(filePath, "utf8");
+		if( buf.length > 0 )
+		{
+			return true;
+		}
+		return false;
+	}
+}
+
+function normalize(phoneNumber) {
+
+    return phoneNumber.replace(
+      /^[\+\d{1,3}\-\s]*\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/,
+      "$1$2$3"
+    );
+
+}
+
+function format(phoneNumber, formatString, options) 
+{
+    // Normalize the phone number first unless not asked to do so in the options
+    if (!options || !options.normalize) {
+      phoneNumber = normalize(phoneNumber)
+    };
+
+    for ( var i = 0, l = phoneNumber.length; i < l; i++ ) {
+      formatString = formatString.replace("N", phoneNumber[i]);
+    }
+  
+    return formatString;
+
+}
+
+function blackListNumber(phoneNumber)
+{
+	var num = format(phoneNumber, "(NNN) NNN-NNNN");
+	var area = num.substring(1,4);
+	if( area == "212" )
+	{
+		return true;
+	}
+	return false;
+}
+
+exports.fileTest = fileTest;
+exports.normalize = normalize;
+exports.format = format;
+exports.inc = inc;
+exports.weird = weird;
+exports.blackListNumber = blackListNumber;
+
